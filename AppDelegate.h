eaccb43aeb1bf05fb199720e35b984f8a8b20657
guozhengmao <648074101@qq.com>
2016-10-26 09:27:31 +0800

    create-project
    创建项目

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecordDemo
+//
+//  Created by 郭正茂 on 16/10/26.
+//  Copyright © 2016年 ldjt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecordDemo
+//
+//  Created by 郭正茂 on 16/10/26.
+//  Copyright © 2016年 ldjt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
